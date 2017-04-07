dc1dc912436dbc243f2617fbbf0dd5ede9f55276
msochel <msochel@unal.edu.co>
2017-04-07 18:51:55 -0500

    query consult from bibliography

--- app.py
+++ app.py
@@ -2,14 +2,39 @@
 
 # external libraries
 from sanic import Sanic
-from sanic.response import text
+from sanic.response import text, json
+from condor.dbutil import requires_db
+from condor.models import BibliographySet
+
 
 app = Sanic(__name__)
+
 
 @app.route("/ping")
 async def start(request):
     return text("pong")
 
+
+@app.route("/list")
+@requires_db
+async def join_list(db, request):
+    to_return = [{
+        'eid': bib.eid,
+        'description': bib.description,
+        'created': bib.created,
+        'modified': bib.modified
+    } for bib in list_db(db, count=10)]
+    return json(to_return)
+
+
+def list_db(db, count):
+    """
+    List all the document sets.
+    """
+    bibliography_sets = db.query(BibliographySet).order_by(
+        BibliographySet.created.desc()
+    ).limit(count)
+    return bibliography_sets.all()
 
 if __name__ == "__main__":
     app.run(
