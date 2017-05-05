1341f1252466a945937ad62f153c3f768ec33a63
msochel <msochel@unal.edu.co>
2017-05-05 18:18:45 -0500

    set term document matrix from db

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 from sanic import Sanic
 from sanic.response import text, json
 from condor.dbutil import requires_db
-from condor.models import Bibliography
+from condor.models import Bibliography, TermDocumentMatrix
 
 
 app = Sanic(__name__)
@@ -19,10 +19,10 @@ async def start(request):
 @requires_db
 async def format_list_bibliography(db, request):
     to_return = [{
-        'eid': bib.eid,
-        'description': bib.description,
-        'created': bib.created,
-        'modified': bib.modified
+        "eid": bib.eid,
+        "description": bib.description,
+        "created": bib.created,
+        "modified": bib.modified
     } for bib in list_bibliography_from_db(db, count=10)]
     return json(to_return)
 
@@ -35,6 +35,30 @@ def list_bibliography_from_db(db, count):
         Bibliography.created.desc()
     ).limit(count)
     return bibliography_sets.all()
+
+
+@app.route("/document_matrix")
+@requires_db
+async def format_term_document_matrix(db, request):
+    to_return = [{
+        "bibliography_eid": document.bibliography_eid,
+        "bibliography_options": document.bibliography_options,
+        "processing_options": document.processing_options,
+        "term_list_path": document.term_list_path,
+        "matrix_path": document.matrix_path
+    } for document in list_term_document_matrix_from_db(db, count=10)]
+    return json(to_return)
+
+
+def list_term_document_matrix_from_db(db, count):
+    """
+    Lis all the term document matrix
+    """
+    document_matrix_sets = db.query(TermDocumentMatrix).order_by(
+        TermDocumentMatrix.created.desc()
+    ).limit(count)
+    return document_matrix_sets.all()
+
 
 if __name__ == "__main__":
     app.run(
