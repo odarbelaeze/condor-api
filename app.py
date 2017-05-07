8201b2d8902fb4cb6f82b295fd149e81c7d35415
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-05-07 16:40:48 -0500

    Add route to list the documents of a bibliography

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 from sanic import Sanic
 from sanic.response import text, json
 from condor.dbutil import requires_db
-from condor.models import Bibliography
+from condor.models import Bibliography, Document
 
 
 app = Sanic(__name__)
@@ -27,6 +27,31 @@ async def format_list_bibliography(db, request):
     return json(to_return)
 
 
+@app.route('/document')
+@requires_db
+async def list_documents(database, request):
+    """
+    List the documents associated with a bibliography.
+    """
+    bibliography_eid = request.args.get('bibliography', None)
+    if not bibliography_eid:
+        return json({
+            'error': 'You must suply a bibliography eid.',
+            'details': 'Fill in the bibliography field.'
+        }, status=400)
+    to_return = [
+        {
+            'eid': doc.eid,
+            'title': doc.title,
+            'description': doc.description,
+            'created': doc.created,
+            'modified': doc.modified
+        }
+        for doc in Document.list(database, bibliography_eid)
+    ]
+    return json(to_return)
+
+
 def list_bibliography_from_db(db, count):
     """
     List all the document sets.
@@ -35,6 +60,7 @@ def list_bibliography_from_db(db, count):
         Bibliography.created.desc()
     ).limit(count)
     return bibliography_sets.all()
+
 
 if __name__ == "__main__":
     app.run(
