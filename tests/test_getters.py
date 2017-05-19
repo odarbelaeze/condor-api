00539894af985eb4255b0729c6a727535d4abf62
Daniel Valencia <dsvalenciah@unal.edu.co>
2017-05-19 18:56:40 -0500

    Add tests for documents, matrices and ranking matrices

--- tests/test_getters.py
+++ tests/test_getters.py
@@ -22,3 +22,19 @@ async def test_can_get_documents(client):
     _, res = client.get('/document?bibliography=123')
     assert res.status == 200
     assert await res.json() == "[]"
+
+
+async def test_get_documents_require_bibliography(client):
+    _, res = client.get('/document')
+    assert res.status == 400
+    assert await res.json() == "[]"
+
+
+def test_can_get_matrices(client):
+    _, res = client.get('/matrix')
+    assert res.status == 200
+
+
+def test_can_get_ranking_matrices(client):
+    _, res = client.get('/ranking')
+    assert res.status == 200
