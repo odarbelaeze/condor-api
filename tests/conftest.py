839f78504bae7115073b8e3b6c39df19d4d56528
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-05-26 23:15:00 -0500

    Make the tests actually run

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+import json
+import pytest
+
+from sanic.testing import SanicTestClient
+
+
+class DecoratedResponse(object):
+    def __init__(self, response):
+        self._response = response
+    def json(self):
+        return json.loads(self._response.body)
+    def __getattr__(self, name):
+        return getattr(self._response, name)
+
+
+class WrappedSanicTestClient(SanicTestClient):
+    def _sanic_endpoint_test(self, *args, **kwargs):
+        req, res = super()._sanic_endpoint_test(*args, **kwargs)
+        return req, DecoratedResponse(res)
+
+
+
+@pytest.fixture()
+def client():
+    from app import app
+    return WrappedSanicTestClient(app)
--- tests/test_getters.py
+++ tests/test_getters.py
@@ -2,39 +2,30 @@
 Tests related to get lists of stuffs.
 """
 
-
-import pytest
-
-from app import app
-
-
-@pytest.fixture()
-def client():
-    return app.test_client
-
-
 def test_can_get_bibliographies(client):
     _, res = client.get('/bibliography')
     assert res.status == 200
 
 
-async def test_can_get_documents(client):
+def test_can_get_documents(client):
     _, res = client.get('/document?bibliography=123')
     assert res.status == 200
-    assert await res.json() == "[]"
+    assert res.json() == []
 
 
-async def test_get_documents_require_bibliography(client):
+def test_get_documents_require_bibliography(client):
     _, res = client.get('/document')
     assert res.status == 400
-    assert await res.json() == "[]"
+    assert 'error' in res.json()
 
 
 def test_can_get_matrices(client):
     _, res = client.get('/matrix')
     assert res.status == 200
+    assert res.json() == []
 
 
 def test_can_get_ranking_matrices(client):
     _, res = client.get('/ranking')
     assert res.status == 200
+    assert res.json() == []
