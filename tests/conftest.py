7f7c6a50e9a8ebd8d9bcf91ce625169dd524e7c7
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-05-28 11:09:37 -0500

    Connect the database through sessions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,12 @@
 import json
 import pytest
+import os
 
+from condor.config import DEFAULT_DB_PATH
+from condor.models.base import DeclarativeBase
 from sanic.testing import SanicTestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 
 class DecoratedResponse(object):
@@ -22,5 +27,45 @@ class WrappedSanicTestClient(SanicTestClient):
 
 @pytest.fixture()
 def client():
+    """
+    A simple test client wrapper.
+    """
     from app import app
     return WrappedSanicTestClient(app)
+
+
+@pytest.fixture(scope='session')
+def engine():
+    """
+    Creates an engine to handle our database.
+    """
+    # Use a sqlite database by default.
+    default_url = 'sqlite:///' + os.path.join(DEFAULT_DB_PATH, 'condor.db.test')
+    _engine = create_engine(default_url)
+    DeclarativeBase.metadata.drop_all(_engine)
+    DeclarativeBase.metadata.create_all(_engine)
+    return _engine
+
+
+@pytest.fixture()
+def session(engine):
+    """
+    Creates a session maker instance attached to our default engine.
+    """
+    _session = scoped_session(sessionmaker(bind=engine))
+    yield _session
+    _session.rollback()
+
+
+@pytest.fixture(autouse=True)
+def fresh_database(session, monkeypatch):
+    """
+    Use a fresh database everytime tests run.
+    """
+    def mock_requires_db(func):
+        def wrapper(*args, **kwargs):
+            db = session()
+            monkeypatch.setattr(db, 'commit', db.flush)
+            return func(db, *args, **kwargs)
+        return wrapper
+    monkeypatch.setattr('condor.dbutil.requires_db', mock_requires_db)
--- tests/test_getters.py
+++ tests/test_getters.py
@@ -2,6 +2,9 @@
 Tests related to get lists of stuffs.
 """
 
+from condor.models import Document, Bibliography
+
+
 def test_can_get_bibliographies(client):
     _, res = client.get('/bibliography')
     assert res.status == 200
@@ -29,3 +32,27 @@ def test_can_get_ranking_matrices(client):
     _, res = client.get('/ranking')
     assert res.status == 200
     assert res.json() == []
+
+
+def test_document_endpoint_actually_returns_documents(client, session):
+    # Given some records matching records in the database
+    bib = Bibliography(eid='123', description='lorem')
+    session.add(bib)
+    session.flush()
+    doc = Document(
+        eid='345',
+        bibliography_eid='123',
+        title='lorem',
+        keywords='{}',
+        description='lorem',
+        language='english',
+        hash='alksdjksjdf'
+    )
+    session.add(doc)
+    session.commit()
+    # When I request the documents with the given bibliography eid
+    _, res = client.get('/document?bibliography=123')
+    # Then I receive a successful response
+    assert res.status == 200
+    # And the response is non empty
+    assert len(res.json()) > 0
