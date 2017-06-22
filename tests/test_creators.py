126c58d78360e69c2d16a40f9396a8158844e2b1
dsvalenciah <dsvalenciah@unal.edu.co>
2017-06-22 02:32:17 -0500

    Create test for matrix post endpoint

--- tests/test_creators.py
+++ tests/test_creators.py
@@ -1,12 +1,20 @@
 """
 Test the post methods.
 """
+from condor.models import Bibliography
 
 
-def test_matrix_creation_endpoint(client):
+def test_matrix_creation_endpoint(client, session):
+    bib = Bibliography(eid='123', description='lorem')
+    session.add(bib)
+    session.flush()
+
     response = client.post('/matrix', {
-        'bibliography': '12312312',
+        'bibliography': '123',
         'fields': 'title,description',
     })
-    print(response.json())
+
+    response = client.get(f"/matrix/{response.json().get('eid')}")
+
     assert response.status_code == 200
+    assert response.json().get('bibliography_eid') == '123'
